387d4809e57866348a981c8063543ed90e64c10f
Sam So <v176sam@cathaypacific.com>
2022-09-07 17:49:58 +0800

    Fix header of CORS

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ app.use(bodyParser.json()); // extract json data
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
-    return res.status(200).json({});
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
   }
+  next();
 });
 
 // Use Routes
